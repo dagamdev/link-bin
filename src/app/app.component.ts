f6b478e2fd315ba5c8ddc4f8fe4bcbaa54d4e45c
Dagadev <gacub20ru@gmail.com>
2024-03-08 16:30:00 -0600

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LinkComponent } from './icons/link/link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LinkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'link-bin';
+  title = 'link-bin'
 }
---
+++ src/app/icons/link/link.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'link-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './link.component.svg'
+})
+export class LinkComponent {
+
+}
