336a382c68cff46e08ebbf8d264f779aa032dcee
Dagadev <gacub20ru@gmail.com>
2024-03-10 13:04:01 -0600

    Add default layout

---
+++ src/app/shared/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { LinkComponent } from '@/icons/link/link.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [LinkComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
