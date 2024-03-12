77850a4facae96837b30f9cd823de9b674af700e
Dagadev <gacub20ru@gmail.com>
2024-03-12 12:56:38 -0600

    Add home, bins and not found pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { NotFoundComponent } from './not-found/not-found.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.routes') },
+  { path: 'bins', loadChildren: () => import('./pages/bins/bins.routes') },
+  { 
+    title: 'Página no encontrada | Link bin',
+    path: '**', 
+    component: NotFoundComponent
+  }
+]
