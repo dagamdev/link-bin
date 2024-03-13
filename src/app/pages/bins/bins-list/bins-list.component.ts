e65ad70793f0ce436576875480cd0b29fa6f1170
Dagadev <gacub20ru@gmail.com>
2024-03-13 16:45:13 -0600

    Add bins list

---
+++ src/app/pages/bins/bins-list/bins-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-bins-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './bins-list.component.html',
+  styleUrl: './bins-list.component.css'
+})
+export class BinsListComponent {
+
+}
