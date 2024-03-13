55713f7e525ca16aa88f3e7f7e57d14ddfb6aa98
Dagadev <gacub20ru@gmail.com>
2024-03-12 18:42:36 -0600

    Add imports

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,13 +1,15 @@
-import { LinkComponent } from '@/icons/link/link.component';
-import { Component } from '@angular/core';
+import { LinkComponent } from '@/icons/link/link.component'
+import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { SidebarComponent } from '../sidebar/sidebar.component'
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [LinkComponent],
+  imports: [RouterLink, LinkComponent, SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
+  
 }
