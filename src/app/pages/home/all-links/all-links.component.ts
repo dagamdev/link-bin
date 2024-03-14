4fde341dc9bac15fea2c86ec0ab57a17366544f7
Dagadev <gacub20ru@gmail.com>
2024-03-13 19:11:28 -0600

    Use link list component

--- src/app/pages/home/all-links/all-links.component.ts
+++ src/app/pages/home/all-links/all-links.component.ts
@@ -3,12 +3,12 @@ import { LayoutComponent } from '@/shared/layout/layout.component'
 import { LinkComponent } from '@/icons/link/link.component'
 import { LinkService } from '@/core/services/link.service'
 import type { Link } from '@/core/models/link'
-import { LinkCardComponent } from '@/shared/link-card/link-card.component'
+import { LinkListComponent } from '@/shared/link-list/link-list.component'
 
 @Component({
   selector: 'app-all-links',
   standalone: true,
-  imports: [LayoutComponent, LinkComponent, LinkCardComponent],
+  imports: [LayoutComponent, LinkComponent, LinkListComponent],
   templateUrl: './all-links.component.html',
   styleUrl: './all-links.component.css'
 })
@@ -16,10 +16,7 @@ export class AllLinksComponent {
   links = signal<Link[]>([])
 
   constructor (private linkService: LinkService) {
-    linkService.links.subscribe(v => {
-      this.links.set(v)
-      console.log(v)
-    })
+    linkService.links.subscribe(this.links.set)
   }
 
   createLink() {
