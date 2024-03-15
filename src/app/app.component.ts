202c016134dbac3f587e12ef1ce93b2b9a3353d9
Dagadev <gacub20ru@gmail.com>
2024-03-15 11:43:19 -0600

    Use modal service & Add loading dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LinkComponent } from './icons/link/link.component';
+import { Component, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { LinkComponent } from './icons/link/link.component'
+import { ModalComponent } from './modal/modal.component'
+import { ModalService } from './core/services/modal.service'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LinkComponent],
+  imports: [RouterOutlet, LinkComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  loadModal = signal(false)
 
+  constructor (private modalService: ModalService) {
+    modalService.load$.subscribe(this.loadModal.set)
+  }
 }
