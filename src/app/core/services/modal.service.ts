3bb8be65d3bf711d58233e93688b358ab64b0ab4
Dagadev <gacub20ru@gmail.com>
2024-03-15 21:26:54 -0600

    Add logic

--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject, Subject } from 'rxjs'
+import type { Modal } from '../models/modal'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
+  private modalState = new Subject<Modal>()
+  modalState$ = this.modalState.asObservable()
+  private load = new BehaviorSubject(false)
+  load$ = this.load.asObservable()
 
   constructor() { }
+
+  openModal (modalData: Omit<Modal, 'show'>) {
+    this.modalState.next({
+      ...modalData,
+      show: true
+    })
+  }
+  
+  closeModal () {
+    this.modalState.next({
+      type: 'create',
+      target: 'bin',
+      show: false
+    })
+  }
+
+  loadModal (){
+    this.load.next(true)
+  }
 }
