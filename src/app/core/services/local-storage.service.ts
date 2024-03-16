8c111d176a1f3bc1565d7eefcf2a005ccc16fa28
Dagadev <gacub20ru@gmail.com>
2024-03-16 10:02:53 -0600

    Add get, update, and delete methods

--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -4,6 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LocalStorageService {
+  noLocalStorage = typeof localStorage === 'undefined'
 
   constructor() { }
+
+  get<T = any> (key: string): T | undefined {
+    if (this.noLocalStorage) return undefined
+
+    const strData = localStorage.getItem(key)
+
+    return strData ? JSON.parse(strData) : undefined
+  }
+
+  update<T = any> (key: string, updatedData: T) {
+    if (this.noLocalStorage) return
+
+    localStorage.setItem(key, JSON.stringify(updatedData))
+  }
+
+  delete (key: string) {
+    if (this.noLocalStorage) return
+
+    localStorage.removeItem(key)
+  }
 }
