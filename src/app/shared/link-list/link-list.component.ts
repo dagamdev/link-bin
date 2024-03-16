91c45ae22189f4492338164d063850ff624e4f5e
Dagadev <gacub20ru@gmail.com>
2024-03-16 09:29:34 -0600

    Add modalService & Add loadModal & Add createLink

--- src/app/shared/link-list/link-list.component.ts
+++ src/app/shared/link-list/link-list.component.ts
@@ -1,14 +1,29 @@
 import { Component, Input, signal } from '@angular/core'
 import { LinkCardComponent } from '../link-card/link-card.component'
 import type { Link } from '@/core/models/link'
+import { ModalService } from '@/core/services/modal.service'
+import { PlusComponent } from '@/icons/plus/plus.component'
 
 @Component({
   selector: 'app-link-list',
   standalone: true,
-  imports: [LinkCardComponent],
+  imports: [LinkCardComponent, PlusComponent],
   templateUrl: './link-list.component.html',
   styleUrl: './link-list.component.css'
 })
 export class LinkListComponent {
   @Input({required: true}) links = signal<Link[]>([])
+
+  constructor (private modalService: ModalService) {}
+
+  loadModal () {
+    this.modalService.loadModal()
+  }
+
+  createLink () {
+    this.modalService.openModal({
+      type: 'create',
+      target: 'link'
+    })
+  }
 }
