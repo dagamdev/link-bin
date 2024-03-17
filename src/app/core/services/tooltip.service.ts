ab64ee24e7c22686751cad45a6c6c9f43535df56
Dagadev <gacub20ru@gmail.com>
2024-03-17 17:57:14 -0600

    Initialize service

--- src/app/core/services/tooltip.service.ts
+++ src/app/core/services/tooltip.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import type { Tooltip } from '../models/tooltip'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TooltipService {
+  data = new BehaviorSubject<Tooltip | undefined>(undefined)
+  data$ = this.data.asObservable()
 
   constructor() { }
+
+  onMouseEnter (event: MouseEvent) {
+
+  }
+
+  onMouseDown () {
+
+  }
+
+  onContextMenu (event: MouseEvent) {
+
+  }
 }
