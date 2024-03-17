314ea494aea30eaa675493cca5ef51c151328ad5
Dagadev <gacub20ru@gmail.com>
2024-03-17 17:59:31 -0600

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router'
 import { LinkComponent } from './icons/link/link.component'
 import { ModalComponent } from './modal/modal.component'
 import { ModalService } from './core/services/modal.service'
+import { TooltipService } from './core/services/tooltip.service'
+import type { Tooltip } from './core/models/tooltip'
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,10 @@ import { ModalService } from './core/services/modal.service'
 })
 export class AppComponent {
   loadModal = signal(false)
+  tooltip = signal<Tooltip | undefined>(undefined)
 
-  constructor (private modalService: ModalService) {
+  constructor (private modalService: ModalService, private tooltipService: TooltipService) {
     modalService.load$.subscribe(this.loadModal.set)
+    tooltipService.data$.subscribe(this.tooltip.set)
   }
 }
