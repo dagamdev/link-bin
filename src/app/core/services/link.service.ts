c0e591d68f67610c591f4b5fd5fd8a23b9f8df23
Dagadev <gacub20ru@gmail.com>
2024-03-17 13:16:10 -0600

    Use localStorage service

--- src/app/core/services/link.service.ts
+++ src/app/core/services/link.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import type { Link } from '../models/link'
 import { BehaviorSubject } from 'rxjs'
+import { LocalStorageService } from './local-storage.service'
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,31 @@ export class LinkService {
   private links = new BehaviorSubject<Link[]>([])
   links$ = this.links.asObservable()
 
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService) {
+    this.links.next(localStorageService.get('links') ?? [])
+  }
   
   create (linkData: Omit<Link, 'id'>) {
     const newLink: Link = {
       id: crypto.randomUUID(),
       ...linkData
     }
+    const updatedData = [...this.links.value, newLink]
 
-    this.links.next([...this.links.value, newLink])
+    this.links.next(updatedData)
+    this.localStorageService.update('links', updatedData)
 
     return newLink
+  }
+
+  delete (linkId: string) {
+    const existLink = this.links.value.some(l=> l.id === linkId)
+
+    if (!existLink) return
+
+    const updatedData = this.links.value.filter(l => l.id !== linkId)
+
+    this.links.next(updatedData)
+    this.localStorageService.update('links', updatedData)
   }
 }
