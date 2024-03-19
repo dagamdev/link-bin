cfc7dc0cc22b618bdafee8a362436d64ef79aea4
Dagadev <gacub20ru@gmail.com>
2024-03-18 22:37:13 -0600

    Add properties & Add types

--- src/app/core/models/modal.ts
+++ src/app/core/models/modal.ts
@@ -1,5 +1,11 @@
 export interface Modal {
-  type: 'create' | 'update'
+  type: 'create' | 'update' | 'confirm'
   target: 'bin' | 'link'
   show: boolean
+  confirmText?: string
+  confirmAction?: () => void
+  defaultData?: FormData
+  updateElementId?: string
 }
+
+type FormData = Partial<Record<'url' | 'name' | 'emoji' | 'color' | 'binId' | 'description', string>>
