8a8a766c1265aaecf0779ab3252fa4baa3729ec6
Dagadev <gacub20ru@gmail.com>
2024-03-23 12:02:02 -0600

    Add header nav component

---
+++ src/app/shared/header-nav/header-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+
+@Component({
+  selector: 'app-header-nav',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './header-nav.component.html',
+  styleUrl: './header-nav.component.css'
+})
+export class HeaderNavComponent {
+
+}
