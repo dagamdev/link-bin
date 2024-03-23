dc2e590bdc6a833b0632f3d9565a707e10d1e445
Dagadev <gacub20ru@gmail.com>
2024-03-23 12:10:30 -0600

    Add page route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NotFoundComponent } from './not-found/not-found.component'
 export const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.routes') },
   { path: 'bins', loadChildren: () => import('./pages/bins/bins.routes') },
+  { path: 'blog', loadChildren: () => import('./pages/blog/blog.routes') },
   { 
     title: 'Página no encontrada | Link bin',
     path: '**', 
