c68a3e5a1ae4a0920a02357137dcfef58064d8d9
Dagadev <gacub20ru@gmail.com>
2024-03-24 13:39:20 -0600

    Add header nav import

--- src/app/pages/blog/page/page.component.ts
+++ src/app/pages/blog/page/page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { HeaderNavComponent } from '@/shared/header-nav/header-nav.component'
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderNavComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
 })
